6b7a1ac36a2657a3048839329f8112a40713e0d7
EduardoCM <ecm3198@gmail.com>
2024-02-05 15:57:37 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [nombre, setNombre] = useState('Sin captura de nombre');
+  const [edad, setEdad] = useState(0);
+
+
+  const [saludo, setSaludo] = useState('Sin confirmacion de registro');
+
+  function evtInputNombre(e){
+    setNombre(e.target.value);
+  }
+
+  function evtInputEdad(e){
+    setEdad(e.target.value);
+  }
+
+  const saludar = () => {
+    console.log("Bienvenido al taller de Java Full Stack con Spring Boot 3 y React");
+    console.log("Nombre: " + nombre);
+    console.log("Edad: "+ edad);
+
+
+    axios.get('http://localhost:8080/hello/'+ nombre +'/' + edad)
+    .then(resultado => {
+       console.log('Resultado: ' + resultado.data);
+       setSaludo(resultado.data);
+    }).catch(error => {
+      console.log(error);
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World - Java Full Stack Developer - React y Spring Boot 3</h1>
+      <br/>
+      Ingresa tu nombre: <input name="nombre" type="text" value={nombre} onChange={evtInputNombre} />
+      <br/>
+      Ingresa tu edad: <input name="edad" type="text" value={edad} onChange={evtInputEdad}/>
+      <br/>
+      <button onClick={() => saludar()}>Registrarme al taller Java Full Stack Developer</button>
+
+      <br/>
+      <strong>{saludo}</strong>
+
     </div>
   );
 }
